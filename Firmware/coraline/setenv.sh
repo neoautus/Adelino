382e2d6bc710e82ac01eb9480a7e9ed3c1da9f57
marcond <marcond@gmail.com>
2018-11-05 18:46:28 -0200

    Add Coraline bootloader wip.

---
+++ Firmware/coraline/setenv.sh
@@ -0,0 +1,6 @@
+# Edit this file to suit your system
+export AVR_GCC_HOME=$HOME/.arduino15/packages/arduino/tools/avr-gcc/5.4.0-atmel3.6.1-arduino2
+export PATH=$AVR_GCC_HOME/bin:$PATH
+export DEVICE_VID=0x2341
+export DEVICE_PID=0x0036
+
---
+++ Firmware/get_lufa.sh
@@ -0,0 +1,20 @@
+cat << EOF
+=======================================================================
+This script downloads and extracts the LUFA library needed by Coraline.
+
+LUFA is the Lightweight USB Framework for AVRs, written by Dean Camera.
+You can find it in: https://github.com/abcminiuser/lufa
+Official site: http://www.lufa-lib.org/
+=======================================================================
+EOF
+wget https://github.com/abcminiuser/lufa/archive/LUFA-111009.tar.gz
+# Remove old LUFA directory
+if [ -e lufa-LUFA-111009 ]; then
+  rm -rf lufa-LUFA-111009
+fi
+tar xf LUFA-111009.tar.gz
+# Preserve the source tar inside LUFA directory
+mv LUFA-111009.tar.gz lufa-LUFA-111009
+echo --------------------------------------
+echo LUFA library downloaded and extracted.
+echo --------------------------------------
