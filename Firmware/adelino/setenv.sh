25025bbaa1b5aaf553adbad4fee40c402ce63f57
marcond <marcond@gmail.com>
2019-01-05 15:54:12 -0200

    Adopt valid USB VID/PID provided by Microchip Inc.

--- Firmware/adelino/setenv.sh
+++ Firmware/adelino/setenv.sh
@@ -1,6 +1,13 @@
 # Edit this file to suit your system
 export AVR_GCC_HOME=$HOME/.arduino15/packages/arduino/tools/avr-gcc/5.4.0-atmel3.6.1-arduino2
 export PATH=$AVR_GCC_HOME/bin:$PATH
-export DEVICE_VID=0x2341
-export DEVICE_PID=0x0036
+
+# Adelino VID/PID kindly provided by Microchip Technology Inc.
+# Please respect the terms of use available at:
+#   http://ww1.microchip.com/downloads/cn/DeviceDoc/cn025058.pdf
+# If you use Microchip devices (like ATmega32U4) you can also
+# get your own free PID, by visiting:
+#   https://www.microchip.com/usblicensing/
+export DEVICE_VID=0x04D8
+export DEVICE_PID=0xEDDF
 
